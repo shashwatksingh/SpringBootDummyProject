6a61a8f773fe512e951b91559cf0980bb0a05435
shashwatksingh <shashwatksingh.27@gmail.com>
2022-09-14 12:04:11 +0530

    using properties and profiles

--- src/main/java/com/shashwatksingh/project1/controllers/Home.java
+++ src/main/java/com/shashwatksingh/project1/controllers/Home.java
@@ -1,14 +1,20 @@
 package com.shashwatksingh.project1.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class Home {
+
+    @Value("${some.config}")
+    private String configString;
     @GetMapping(path = "/")
 //    @RequestMapping(path = "/", method = RequestMethod.GET)
     public String helloWorld() {
-        return "Hello World with an update!";
+        return "Hello World with an update!" + configString;
     }
 
 }
